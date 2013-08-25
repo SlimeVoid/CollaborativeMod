518330cb77ba7822d7aab9251659cd3796604140
Eurymachus <reflex_ion@hotmail.com>
2013-08-25 17:56:05 +0100

    Fix matchOre

--- PB-source/slimevoid/projectbench/core/lib/ItemLib.java
+++ PB-source/slimevoid/projectbench/core/lib/ItemLib.java
@@ -8,7 +8,7 @@ public class ItemLib {
 	public static boolean matchOre(ItemStack a, ItemStack b) {
 		String s1 = OreDictionary.getOreName(OreDictionary.getOreID(a));
 		String s2 = OreDictionary.getOreName(OreDictionary.getOreID(b));
-		if (s1 != null && s2 != null && s1.equals(s2)) {
+		if (s1 != null && !s1.equals("Unknown") && s2 != null && !s2.equals("Unknown") && s1.equals(s2)) {
 			return true;
 		} else {
 			return compareItemStack(a, b) == 0;

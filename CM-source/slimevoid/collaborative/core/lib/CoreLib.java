b1192b79c056872b147290e8501c2a5ad4dde61b
Eurymachus <reflex_ion@hotmail.com>
2013-09-17 21:24:57 +0100

    Version 0.0.0.3

--- CM-source/slimevoid/collaborative/core/lib/CoreLib.java
+++ CM-source/slimevoid/collaborative/core/lib/CoreLib.java
@@ -9,7 +9,7 @@ public class CoreLib {
 	public static final String MOD_ID = "CollaborativeMod";
 	public static final String MOD_RESOURCES = "collaborative";
 	public static final String MOD_NAME = "Collaborative Mod";
-	public static final String MOD_VERSION = "0.0.0.2";
+	public static final String MOD_VERSION = "0.0.0.3";
 	public static final String MOD_DEPENDENCIES = "required-after:SlimevoidLib";
 	public static final String MOD_CHANNEL = "COLLABORATIVE";
 	public static final String CLIENT_PROXY = "slimevoid.collaborative.client.proxy.ClientProxy";

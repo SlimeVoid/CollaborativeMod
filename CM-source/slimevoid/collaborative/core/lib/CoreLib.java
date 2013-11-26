a874942a6aed737d2ecb8016133e1419662f9b97
Allen Cook <alcook16@gmail.com>
2013-11-26 10:53:17 -0500

    Add version to required mod

--- CM-source/slimevoid/collaborative/core/lib/CoreLib.java
+++ CM-source/slimevoid/collaborative/core/lib/CoreLib.java
@@ -10,7 +10,7 @@ public class CoreLib {
 	public static final String	MOD_RESOURCES		= "collaborative";
 	public static final String	MOD_NAME			= "Collaborative Mod";
 	public static final String	MOD_VERSION			= "0.0.1.0";
-	public static final String	MOD_DEPENDENCIES	= "required-after:SlimevoidLib";
+	public static final String	MOD_DEPENDENCIES	= "required-after:SlimevoidLib@[2.0.3.1]";
 	public static final String	MOD_CHANNEL			= "COLLABORATIVE";
 	public static final String	CLIENT_PROXY		= "slimevoid.collaborative.client.proxy.ClientProxy";
 	public static final String	COMMON_PROXY		= "slimevoid.collaborative.proxy.CommonProxy";

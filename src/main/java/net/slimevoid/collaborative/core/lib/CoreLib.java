d1759df5da6133f88fca5bde08ec167469f0e302
Eurymachus <reflex_ion@hotmail.com>
2014-06-06 14:23:13 +0100

    Build Version update

--- src/main/java/net/slimevoid/collaborative/core/lib/CoreLib.java
+++ src/main/java/net/slimevoid/collaborative/core/lib/CoreLib.java
@@ -6,14 +6,15 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class CoreLib {
 
-    public static final String MOD_ID             = "collaborative";
-    public static final String MOD_NAME           = "Collaborative Mod";
-    public static final String MOD_VERSION        = "@VERSION@";
-    public static final String MOD_DEPENDENCIES   = "required-after:SlimevoidLib";
-    public static final String MOD_CHANNEL        = "COLLABORATIVE";
-    public static final String CLIENT_PROXY       = "net.slimevoid.collaborative.client.proxy.ClientProxy";
-    public static final String COMMON_PROXY       = "net.slimevoid.collaborative.proxy.CommonProxy";
+    public static final String MOD_ID              = "collaborative";
+    public static final String MOD_NAME            = "Collaborative Mod";
+    public static final String MOD_VERSION         = "@VERSION@";
+    public static final String MOD_DEPENDENCIES    = "";
+    public static final String ACTUAL_DEPENDENCIES = "required-after:Forge@[@FORGEVERSION@,),SlimevoidLib@[@SLIMEVERSION@,)";
+    public static final String MOD_CHANNEL         = "COLLABORATIVE";
+    public static final String CLIENT_PROXY        = "net.slimevoid.collaborative.client.proxy.ClientProxy";
+    public static final String COMMON_PROXY        = "net.slimevoid.collaborative.proxy.CommonProxy";
     @SideOnly(Side.CLIENT)
-    public static boolean      OPTIFINE_INSTALLED = FMLClientHandler.instance().hasOptifine();
+    public static boolean      OPTIFINE_INSTALLED  = FMLClientHandler.instance().hasOptifine();
 
 }
